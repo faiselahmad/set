021e35fb5568ebc984c9784d3a2865c4c8b19496
Hp <Hp@192.168.0.104>
2019-09-16 17:15:43 +0530

    First Commit Set

---
+++ SetInterface/src/TreeSetEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TreeSetEx {
+ public static void main(String args[]) {
+	  TreeSet ts =new TreeSet();
+	  ts.add("C");
+	  ts.add("A");
+	  ts.add("B");
+	  ts.add("E");
+	  ts.add("F");
+	  ts.add("D");
+	  System.out.println(ts);
+	  
+ }
+}
